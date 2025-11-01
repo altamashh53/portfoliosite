db444b0fc583c60ec025ba010d702442f804dee7
altamashh53 <notsocialfr@gmail.com>
2025-11-01 19:04:42 +0530

    minor  copy

--- src/components/FinalCTA/FinalCTA.tsx
+++ src/components/FinalCTA/FinalCTA.tsx
@@ -16,10 +16,10 @@ const FinalCTA: React.FC = () => {
             viewport={{ once: true }}
             className="text-3xl md:text-4xl font-bold text-white mb-6 tracking-tight"
           >
-            It's simple, yet most businesses think it's not worth it -
+            It's simple, yet most businesses think it's not worth it.
             <br /> <br />
-            That's why <i> they struggle</i> &{" "}
-           <span className="text-[#8CEAB3]"> <u> <b> we shine.</b> </u></span>
+            That's why <i> most struggle</i> &{" "}
+           <span className="text-[#8CEAB3]"> <u> <b> a few shine.</b> </u></span>
           </motion.h2>
           <motion.p
             initial={{ opacity: 0, y: 20 }}
@@ -28,8 +28,7 @@ const FinalCTA: React.FC = () => {
             transition={{ delay: 0.1 }}
             className="text-white text-opacity-60 mb-8"
           >
-            Get a Free Audit That Shows Exactly How Many Customers You're Losing
-            - And How We Can Fix It.
+            Book a free audit of your business and see where you're losing customers.
           </motion.p>
           <ContactForm2 />
         </div>
